246f6b08b5121989f89a5ce2914a03a2ba22b4b7
martin ogbemudia <martindev52@gmail.com>
2024-03-19 23:17:12 -0600

    updated some index and some css file and also deleted vercel.json
    added a background remove in the menu bar and also made it responsive

--- assets/booking/booking.js
+++ assets/booking/booking.js
@@ -1,5 +1,22 @@
 
 AOS.init({
-    duration: 3000,
+    duration: 2000,
     once: true,
 });
+
+const icon = document.querySelector(".fa-solid");
+const links = document.querySelector(".header-list");
+const background = document.querySelector(".body");
+// const iconss = document.querySelector(".fa-xmark")
+console.log(links);
+icon.addEventListener('click', ()=>{
+    links.classList.toggle("active");
+    icon.classList.toggle('fa-xmark')
+    background.classList.toggle("open");
+})
+
+background.addEventListener('click', () =>{
+    icon.classList.remove('fa-xmark')
+    links.classList.remove('active')
+    background.classList.remove('open')
+ })
--- assets/payment/payment.js
+++ assets/payment/payment.js
@@ -22,3 +22,20 @@ function loading(){
     window.location.href = '../success/success.html'
    },2100)
 }
+
+const icon = document.querySelector(".fa-solid");
+const links = document.querySelector(".header-list");
+const background = document.querySelector(".body");
+// const iconss = document.querySelector(".fa-xmark")
+console.log(links);
+icon.addEventListener('click', ()=>{
+    links.classList.toggle("active");
+    icon.classList.toggle('fa-xmark')
+    background.classList.toggle("open");
+})
+
+background.addEventListener('click', () =>{
+   icon.classList.remove('fa-xmark')
+   links.classList.remove('active')
+   background.classList.remove('open')
+})
